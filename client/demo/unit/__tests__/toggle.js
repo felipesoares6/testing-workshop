f68350e3eb75eca5aa6521974b85b86ff63d7e88
felipe.soares <felipeluizsoares@gmail.com>
2018-01-23 23:31:56 -0200

    Add unit tests to toggle component

--- client/demo/unit/__tests__/toggle.js
+++ client/demo/unit/__tests__/toggle.js
@@ -1,1 +1,17 @@
-test('todo', () => {})
+import React from 'react'
+import {shallow, render} from 'enzyme'
+import Toggle from '../toggle'
+
+test('the component renders with defaults', () => {
+  const wrapper = render(<Toggle onToggle={() => {}}> CHILD HERE </Toggle>)
+
+  expect(wrapper).toMatchSnapshotWithGlamor()
+})
+
+test('the onToggle function is called when the button is clicked', () => {
+  const onToggle = jest.fn()
+  const wrapper = shallow(<Toggle onToggle={onToggle}> CHILD HERE </Toggle>)
+
+  wrapper.find('[data-test="button"]').simulate('click')
+  expect(onToggle).toHaveBeenCalledTimes(1)
+})
