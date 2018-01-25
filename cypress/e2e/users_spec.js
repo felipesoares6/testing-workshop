8c1a6abea54474c478d490aec98352703cfa1492
felipe.soares <felipeluizsoares@gmail.com>
2018-01-25 00:54:14 -0200

    Add e2e tests to user

--- cypress/e2e/users_spec.js
+++ cypress/e2e/users_spec.js
@@ -1,7 +1,42 @@
-import {sel} from '../utils'
+import {sel, getRandomUserData, createNewUser} from '../utils'
 describe('Users', () => {
-  // what kinds of things does a user do that we want to make sure
-  // doesn't break?
+  it('should allow a new user to sing up and log out', () => {
+    const {username, email, password} = getRandomUserData()
+
+    cy.visitApp()
+      .get(sel('sign-up-link'))
+      .click()
+      .get(`form ${sel('username')}`)
+      .type(username)
+      .get(`form ${sel('email')}`)
+      .type(email)
+      .get(`form ${sel('password')}`)
+      .type(password)
+      .get('form')
+      .submit()
+
+    verifyLoggedIn(username)
+
+    cy.get(sel('settings')).click().get(sel('logout')).click()
+    cy.window().its('localStorage').invoke('getItem', 'jwt').should('be.null')
+    cy.get(sel('profile-link')).should('not.exist')
+  })
+
+  it('should allow an existing user to login', () => {
+    createNewUser().then(({user: {email, username, password}}) => {
+      cy.visitApp()
+        .get(sel('sign-in-link'))
+        .click()
+        .get(sel('email'))
+        .type(email)
+        .get(sel('password'))
+        .type(password)
+        .get('form')
+        .submit()
+
+      verifyLoggedIn(username)
+    })
+  })
 })
 
 // I'll just give you this function :)
